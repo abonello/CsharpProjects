67417f839ba1f63f3e6d92f4e071c6eaf84def70
Anthony Bonello <anthony@methodworx.com>
2024-10-05 17:48:59 +0100

    Test project

---
+++ validateStringInput/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+Console.WriteLine("Validate String Input\n");
+
+string[] roles = { "administrator", "manager", "user" };
+string? input;
+bool roleIsValid = false;
+
+Console.WriteLine("Enter your role name (Administrator, Manager, or User)");
+
+do
+{
+  input = Console.ReadLine();
+
+  if (!roles.Contains(input.Trim().ToLower()))
+  {
+    Console.WriteLine($"The role name that you entered, \"{input.Trim()}\" is not valid. Enter your role name (Administrator, Manager, or User)");
+    continue;
+  }
+
+  roleIsValid = true;
+
+} while(!roleIsValid);
+
+Console.WriteLine($"Your input value ({input.Trim()}) has been accepted.");
