31a1b512a98e2b397760ace069730850fcad7c36
Anthony Bonello <ab@anthonybonello.co.uk>
2024-10-06 20:19:46 +0100

    explore while loop

---
+++ exploreWhile/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("Explore WHILE Loop");
+
+Random random = new();
+int current = random.Next(1,11);
+
+while (current >= 3)
+{
+  Console.WriteLine(current);
+  current = random.Next(1,11);
+}
+
+System.Console.WriteLine($"Last number: {current}");
