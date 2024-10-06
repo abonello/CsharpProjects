144c9b06ec1f1847ab7798f847e672753af49785
Anthony Bonello <ab@anthonybonello.co.uk>
2024-10-06 20:17:43 +0100

    exolore do while loop

---
+++ exploreDoWhile/Program.cs
@@ -0,0 +1,27 @@
+﻿
+Console.Clear();
+Console.WriteLine("Explore Do While Loop");
+
+Random random = new();
+int current = 0;
+int counter = 1;
+
+do
+{
+  current = random.Next(1, 11);
+  Console.WriteLine($"{counter}:\t{current}");
+  counter++;
+} while (current != 7);
+
+Console.WriteLine("\nUsing Continue\n");
+
+current = random.Next(1, 11);
+
+do
+{
+  current = random.Next(1, 11);
+
+  if (current >= 8) continue;
+
+  Console.WriteLine(current);
+} while (current != 7);
