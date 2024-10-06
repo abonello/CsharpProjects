d094e1a60d59efa2470ab982282eb423382b3ef3
Anthony Bonello <ab@anthonybonello.co.uk>
2024-10-06 14:32:29 +0100

    Add conflip project

---
+++ coinFlip/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("Welcome to the Coin Flip Game!");
+
+Random random = new();
+int flip = random.Next(2);
+string result = flip == 0 ? "Heads" : "Tails";
+
+Console.WriteLine($"The coin landed on: {result}!\n\n");
+
+// Debugging
+Console.WriteLine(flip);
